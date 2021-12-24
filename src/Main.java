2ba726b4f3d623df7e557aca0acc5b78514509c3
Nicholas <n.paz4444@gmail.com>
2021-12-23 22:57:39 -0500

    Successfully completed.

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,79 @@
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        String exampleOne ="()";
-        String exampleTwo ="()[]{}";
-        String exampleThree ="(]";
-        boolean result = isValid(exampleOne);
-        System.out.println("() results in: " + isValid(exampleOne));
-        System.out.println("()[]{} results in: " + isValid(exampleTwo));
-        System.out.println("(] results in: " + isValid(exampleThree));
+        String exampleOne = "{[()]}";
+        String failedExample = "([}}])";
+//        String exampleTwo = "()[]{}";
+//        String exampleThree = "(]";
+//        boolean result = isValid(exampleOne);
+//        System.out.println("() results in: " + isValid(exampleOne));
+//        System.out.println("()[]{} results in: " + isValid(exampleTwo));
+//        System.out.println("(] results in: " + isValid(exampleThree));
+        boolean result = isValid(failedExample);
+
+       // System.out.println("{[()]} should return true: " + isValid(exampleOne));
+        System.out.println("([}}]) should return false: " + isValid(failedExample));
+
+
     }
 
-    public static boolean isValid(String s){
-        // need a flag for result of for loop
-        boolean flag = true;
+    public static boolean isValid(String s) {
 
-        int sizeOfString = s.length();
+        // flag to be returned
+        boolean flag = false;
 
-        // if there is an odd number of character in string, set flag to false
-        if(sizeOfString % 2 != 0){
-            flag = false;
+        // keep track of the amount of characters that are removed
+        int removedCharacterCount = 0;
+        // if s has an odd number of letters, then return false
+        if(s.length()%2 != 0){
+            return false;
+        }
+
+        // we need a stack
+        Stack<Character> stack = new Stack<Character>();
+
+        // iterate through and add the "open" brackets to the stack
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+                stack.push(s.charAt(i));
+            }
+            // if a "closed" character appears, and there is an opening bracket to go with it, pop it from stack
+            // make sure the stack is not empty
+            else if(s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '('){
+                // this will remove the open (
+                stack.pop();
+                removedCharacterCount++;
+            }
+            // check the bracket scenario
+            else if(s.charAt(i) == ']'&& !stack.isEmpty() && stack.peek() == '[' ){
+
+                // this will remove the open [ from the stack
+                stack.pop();
+                removedCharacterCount++;
+            }
+            // check the curley brace scenario
+            else if(s.charAt(i) == '}'&& !stack.isEmpty() && stack.peek() == '{' ){
+                // this will remove the { from the stack
+                stack.pop();
+                removedCharacterCount++;
+            }
+
+        }
+
+        // print for testing purposes
+        //System.out.println(stack.toString());
+        //System.out.println(stack.peek().toString());
+        if(removedCharacterCount * 2 == s.length()){
+            flag = true;
         }
         else{
-            // I want to find the "middle" set of brackets
-            int leftIndex = 0 ;  //need to minus 1 because index starts at zero
-            int rightIndex = leftIndex +1;     // right index should be placed at the correct index by dividing by 2
-
-            // start from the middle and work outwards
-            while(flag && rightIndex <= s.length()-1){
-                flag = parenthesesChecker(s.charAt(leftIndex), s.charAt(rightIndex));
-
-                // need to make sure we do not decrement lower than zero
-                // if flag is still true, decrement leftIndex, increment rightIndex, check again with the next set
-                if(flag){
-                    leftIndex +=2;
-                    rightIndex = leftIndex +1;
-                }
-            }
+            flag = false;
         }
 
-            return flag;
-    }
-
-    public static boolean parenthesesChecker(char lI, char rI){
-        boolean flag = false;
-        // ()
-        if(lI == '(' && rI == ')'){
-            flag = true;
-        }
-
-        //{}
-        else if(lI == '{' && rI == '}'){
-            flag = true;
-        }
-        // []
-        else if(lI == '[' && rI == ']'){
-            flag = true;
-        }
         return flag;
     }
 }
+
